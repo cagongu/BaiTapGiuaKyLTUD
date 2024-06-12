f69874d2d20e601ce5959df5aac9839635e11711
cagongu <hophucthai9@gmail.com>
2024-06-12 13:27:50 +0700

    first commit

---
+++ baitapgiuaky/src/main/java/com/hutech/baitapgiuaky/BaitapgiuakyApplication.java
@@ -0,0 +1,13 @@
+package com.hutech.baitapgiuaky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaitapgiuakyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaitapgiuakyApplication.class, args);
+	}
+
+}
---
+++ baitapgiuaky/src/test/java/com/hutech/baitapgiuaky/BaitapgiuakyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hutech.baitapgiuaky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaitapgiuakyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
